6e25ac6378af177a0d2360a8915c80e96aad984d
Sébastien Reuiller <sebastien@reuiller.email>
2023-02-01 14:33:57 +0100

    add editable body for service

--- services/models.py
+++ services/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
 from wagtail.models import Page
+
+from cms.models import (
+    AlertBlock,
+    CalloutBlock,
+    FaqBlock,
+    ImageAndTextBlock,
+    ImageBlock,
+    MultiColumnsWithTitleBlock,
+    QuoteBlock,
+    StepperBlock,
+    VideoBlock,
+)
 
 
 class ServicePage(Page):
@@ -9,3 +24,28 @@ class ServicePage(Page):
     beta_link = models.URLField("Lien vers le service", max_length=255, blank=True)
     beta_problem = models.TextField("Le problème", blank=True)
     beta_service = models.TextField("Notre service", blank=True)
+
+    body = StreamField(
+        [
+            ("paragraph", blocks.RichTextBlock(label="Texte avec mise en forme")),
+            ("image", ImageBlock()),
+            (
+                "imageandtext",
+                ImageAndTextBlock(label="Bloc image à gauche et texte à droite"),
+            ),
+            ("alert", AlertBlock(label="Message d'alerte")),
+            ("callout", CalloutBlock(label="Texte mise en avant")),
+            ("quote", QuoteBlock(label="Citation")),
+            ("video", VideoBlock(label="Vidéo")),
+            ("multicolumns", MultiColumnsWithTitleBlock(label="Multi-colonnes")),
+            ("faq", FaqBlock(label="Questions fréquentes")),
+            ("stepper", StepperBlock(label="Étapes")),
+        ],
+        blank=True,
+        use_json_field=True,
+    )
+
+    # Editor panels configuration
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
