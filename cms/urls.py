3e5a646dcccb336d450838fa9007160a9e8e0b3e
Sébastien Reuiller <sebastien@reuiller.email>
2023-02-23 09:44:57 +0100

    add sitemap

--- cms/urls.py
+++ cms/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.documents import urls as wagtaildocs_urls
 
 
 urlpatterns = [
     path("cms-admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path("sitemap.xml", sitemap),
     path("", include(wagtail_urls)),
 ]
