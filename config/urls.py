d63de53ad8b0ead0cb08fda1ca7eba233e0f2398
madjid.asa <madjid.asa@gmail.com>
2024-07-10 17:41:15 +0200

    feat: add view to display well kwon config

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
@@ -21,4 +22,5 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("services.urls")),
     path("", include("cms.urls")),
+    path(".well-known/", include("well_known.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
