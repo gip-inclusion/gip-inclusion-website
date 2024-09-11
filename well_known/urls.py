c5d775a3a4d3882c1fb00049738a3ce186edf399
Sébastien Reuiller <sebastien@reuiller.email>
2024-09-11 22:03:53 +0200

    fix quality

--- well_known/urls.py
+++ well_known/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 
-
 from .views import serve_text_file
 
 
--- well_known/views.py
+++ well_known/views.py
@@ -1,12 +1,13 @@
 import os
+
 from django.http import HttpResponse
 
 
 def serve_text_file(request, file_name):
-    file_path = os.path.join('static', '.well-known', file_name)
+    file_path = os.path.join("static", ".well-known", file_name)
     try:
-        with open(file_path, 'rb') as file:
+        with open(file_path, "rb") as file:
             content = file.read()
-        return HttpResponse(content, content_type='text/plain; charset=utf-8')
+        return HttpResponse(content, content_type="text/plain; charset=utf-8")
     except FileNotFoundError:
         return HttpResponse(b"File not found.", status=404)
