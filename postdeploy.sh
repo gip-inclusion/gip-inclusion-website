ad7ff4a6bb98d272288a09dbe56712acacde19ef
Scttpr <charles.capelli.pub@protonmail.com>
2024-10-29 16:36:45 +0100

    fix: script test
    Signed-off-by: Scttpr <charles.capelli.pub@protonmail.com>

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,2 +1,1 @@
-python manage.py migrate
-cd otelcol-contrib && ./otelcol-contrib --config ./config.yaml &> otelcol-output.log & echo "$!" > otel-pid
+python manage.py migrate && cd otelcol-contrib && ./otelcol-contrib --config ./config.yaml &> otelcol-output.log & echo "$!" > otel-pid
