ef660ce06f78fcdbc4928e5ef952f92301ba8fde
Scttpr <charles.capelli.pub@protonmail.com>
2024-10-29 16:31:52 +0100

    fix: script typo
    Signed-off-by: Scttpr <charles.capelli.pub@protonmail.com>

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,2 +1,2 @@
 python manage.py migrate
-cd otecol-contrib && ./otelcol-contrib --config ./config.yaml &> otelcol-output.log & echo "$!" > otel-pid
+cd otelcol-contrib && ./otelcol-contrib --config ./config.yaml &> otelcol-output.log & echo "$!" > otel-pid
