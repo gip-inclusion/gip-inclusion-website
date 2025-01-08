968fb73b58986ddaa77af33f15b519115adad85d
François Freitag <mail@franek.fr>
2025-01-08 09:27:07 +0100

    Fix flake8 issues on main

--- cms/tests.py
+++ cms/tests.py
@@ -10,37 +10,37 @@ class CMSPageTest(TestCase):
 
     def test_title_tag_when_seo_title_is_set(self):
         page = ContentPageFactory(with_seo_title=True, with_search_description=True)
-        response = self.client.get(f"/{ page.slug }/")
+        response = self.client.get(f"/{page.slug}/")
         self.assertContains(
             response,
-            f"<title>{ page.seo_title } — La plateforme de l'inclusion</title>",
+            f"<title>{page.seo_title} — La plateforme de l'inclusion</title>",
             count=1,
             html=True,
         )
 
     def test_title_tag_when_seo_title_is_not_set(self):
         page = ContentPageFactory()
-        response = self.client.get(f"/{ page.slug }/")
+        response = self.client.get(f"/{page.slug}/")
         self.assertContains(
             response,
-            f"<title>{ page.title } — La plateforme de l'inclusion</title>",
+            f"<title>{page.title} — La plateforme de l'inclusion</title>",
             count=1,
             html=True,
         )
 
     def test_meta_description_tag_when_search_description_is_set(self):
         page = ContentPageFactory(with_search_description=True)
-        response = self.client.get(f"/{ page.slug }/")
+        response = self.client.get(f"/{page.slug}/")
         self.assertContains(
             response,
-            f'<meta name="description" content="{ page.search_description }" />',
+            f'<meta name="description" content="{page.search_description}" />',
             count=1,
             html=True,
         )
 
     def test_meta_description_tag_when_search_description_is_not_set(self):
         page = ContentPageFactory()
-        response = self.client.get(f"/{ page.slug }/")
+        response = self.client.get(f"/{page.slug}/")
         self.assertContains(response, '<meta name="description" content="" />', count=1, html=True)
 
 
